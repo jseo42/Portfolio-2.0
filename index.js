cd1be1139230be7a19947bb0ac30154d5253b62e
jseo42 <jayseo.js@gmail.com>
2023-08-23 15:48:41 -0500

    added The Works section

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ text.innerHTML = text.innerHTML.split("").map(
   `<span style="transform:rotate(${i * 8 }deg)">${char}</span>`
 ).join('')
 
+function createProjects() {
+  const projectList = [
+    {name:'Calculator', image:'', github:'https://github.com/jseo42/Calculator', live:'https://htmlpreview.github.io/?https://github.com/jseo42/Calculator/blob/main/index.html'},
+    {name:'Pokedex', image:'', github:'', live:''},
+    {name:'Library App', image:'', github:'', live:''},
+  ]
+}
