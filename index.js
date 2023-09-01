e2621d1f58f35f7593db14ba48487afbcbe348b5
jseo42 <jayseo.js@gmail.com>
2023-09-01 11:02:31 -0500

    deleted code regarding scroll animation

--- index.js
+++ index.js
@@ -3,24 +3,11 @@ window.addEventListener('scroll', function() {
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-const divWatch = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show')
-    }
-    
-  })
-});
-
-const hiddenEl = document.querySelectorAll('.hidden');
-hiddenEl.forEach((el) => divWatch.observe(el));
 
 const text = document.querySelector('.logo-text p');
 text.innerHTML = text.innerHTML.split("").map(
   (char, i) => 
-  `<span style="transform:rotate(${i * 8 }deg)">${char}</span>`
+  `<span style="transform:rotate(${i * 9.0 }deg)">${char}</span>`
 ).join('')
 
 const toggleBtn = document.querySelector('.toggle-btn');
